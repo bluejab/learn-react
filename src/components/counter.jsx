f132e45e327953a8428883072d426fb0aa19dda6
gandhy <gandhybong@gmail.com>
2018-08-07 15:16:04 +0700

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+    tags: []
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.value + 1 });
+  };
+
+  render() {
+    console.log("props", this.props);
+
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* <br />
+        {this.state.tags.length === 0 && "please create a new tag!"}
+        {this.renderTags()} */}
+      </div>
+    );
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+}
+
+export default Counter;
